b0f9a9c420ec7f1f7cf81b7fe973796a60206c8d
mountainvalleycode <none>
2021-03-23 20:51:52 -0500

    announce small battlefield

--- src/Notice.tsx
+++ src/Notice.tsx
@@ -7,6 +7,7 @@ export const Notice = () => {
   return (
     <Card color="rgb(255, 242, 161)">
       <h3>Update: Pyra & Mythra data are here!</h3>
+      <p>Small Battlefield data is also here!</p>
     </Card>
   );
 };
